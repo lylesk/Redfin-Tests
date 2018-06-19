d2aa82dbeb85df01f5c67a64d4307fd47eff5d38
lylesk <40308613+lylesk@users.noreply.github.com>
2018-06-18 20:53:16 -0700

    Removed unnecessary files

--- Zillow/zillow.rb
+++ Zillow/zillow.rb
@@ -1,32 +1,3 @@
-require 'rubygems'
-require 'faraday'
-require 'rspec'
-require 'spec-helpers'
+class ZillowAPI
 
-RSpec.describe Zillow_API do
-	context 'The results of hitting the API'
-	it "should match the address entered in the search" do
-		before do
-		url = 'https://www.zillow.com/webservice/GetSearchResults.htm'
-		end
-		
-	it "should return a 200 response when searching for a specific address"
-	conn = Faraday.new(url: url) do |faraday|
-		faraday.adapter Faraday.default_adapter
-		faraday.response :json
-		conn.get do |req|
-		req.params['zws-id'] ='zws-id=X1-ZWz1gi0bozmebv_9d4gz'
-		req.params['address'] = '1603 Southeast 29th Avenue'
-		req.params['citystatezip'] = 'Portland'
-		response = conn.get
-		expect(response).to eq 200
-		end
-		end
-	
-	it "should return the correct address in the JSON response"
-		expect(response).to eq ('1603 Southeast 29th Avenue')
-		end
-	end
-
-###response.body (data['content'].to eq ('1603 Southeast 29th Avenue'))
-
+end
