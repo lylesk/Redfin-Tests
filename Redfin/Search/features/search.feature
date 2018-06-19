4a9c2bb6b3251e1004632cbcddbd95717542893e
lylesk <40308613+lylesk@users.noreply.github.com>
2018-06-18 19:37:02 -0700

    Changed some stuff around

--- Redfin/Search/features/search.feature
+++ Redfin/Search/features/search.feature
@@ -1,7 +1,8 @@
 Feature: Searches with multiple filters can be done
-Scenario: Multiple fitler search
+Scenario: Search for properties using three filters
     Given I am on the Redfin search screen
+	Given I add "97214" as the zip code
     When I add a filter for max price of "$500000"
-    And I add a filter for min bedrooms of "2"
+    And I add a filter for min bedrooms of 2
 	And I select house as the property type
 	Then I should see three filter options applied to my search
--- Redfin/Search/features/step_definitions/search_steps.rb
+++ Redfin/Search/features/step_definitions/search_steps.rb
@@ -1,28 +1,30 @@
 Given(/I am on the Redfin search screen$/) do
 	visit ('http://www.redfin.com')
 	click ('search-box-input')
-	fill_in 'search-box-input', :with=> '97214'
+end
+Given (/^I add "(.*?)" as the zip code$/) do |zipCode|
+	fill_in 'search-box-input', :with=> '97214'; zipCode
 	click ('searchButton')
 	page.should have_content('97214')
 end
-When(/^I add a filter for max price of "500,000"$/) do
+When(/^I add a filter for max price of "(.*?)"$/) do |price|
 	find_button ('filterButton')
 	click_button ('filterButton')
 	find_select ('maxPrice')
-	fill_in 'maxPrice', :with=> '$500,000'
+	fill_in 'maxPrice', :with=> '$500,000'; price
 	
 end
-When(/^I add a filter for min bedrooms of "2"$/) do
+And(/^I add a filter for min bedrooms of "(.*?)"$/) do 
 	find_select ('minBeds')
-	select('2', :from => 'minBeds')
+	select('2', :from => 'minBeds'); 
 end
-When(/^I select house as the property type$/) do
+And(/^I select house as the property type$/) do
 	find_button ('data-rf-test-name="uipt1"')
 	click_button ('data-rf-test-name="uipt1"')
 	click_button ('apply-search-options')
 end
 Then(/^I should see three filter options applied to my search$/) do
 	page.find('wideSidepaneFilterButtonContainer', '$0-$500k, 2+ beds, house')
-
+end
 	
 	
