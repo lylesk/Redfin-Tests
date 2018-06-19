b3f96f5d7b5d2a641ace6bd399ee5036081b207c
lylesk <40308613+lylesk@users.noreply.github.com>
2018-06-18 19:56:20 -0700

    Mulligan

--- Redfin/Search/features/search.feature
+++ Redfin/Search/features/search.feature
@@ -3,6 +3,6 @@ Scenario: Search for properties using three filters
     Given I am on the Redfin search screen
 	Given I add "97214" as the zip code
     When I add a filter for max price of "$500000"
-    And I add a filter for min bedrooms of 2
+    And I add a filter for min bedrooms of "2"
 	And I select house as the property type
 	Then I should see three filter options applied to my search
--- Redfin/Search/features/step_definitions/search_steps.rb
+++ Redfin/Search/features/step_definitions/search_steps.rb
@@ -1,29 +1,29 @@
-Given(/I am on the Redfin search screen$/) do
+Given (/^I am on the Redfin search screen$/) do
 	visit ('http://www.redfin.com')
 	click ('search-box-input')
 end
-Given (/^I add "(.*?)" as the zip code$/) do |zipCode|
-	fill_in 'search-box-input', :with=> '97214'; zipCode
+Given (/^I add "([^"]*)" as the zip code$/) do |zipCode|
+	fill_in 'search-box-input', :with=> zipCode
 	click ('searchButton')
 	page.should have_content('97214')
 end
-When(/^I add a filter for max price of "(.*?)"$/) do |price|
+When (/^I add a filter for a max price of "([^"]*)"$/) do |price|
 	find_button ('filterButton')
 	click_button ('filterButton')
 	find_select ('maxPrice')
-	fill_in 'maxPrice', :with=> '$500,000'; price
+	fill_in 'maxPrice', :with=> price
 	
 end
-And(/^I add a filter for min bedrooms of "(.*?)"$/) do 
+And (/^I add a filter for min bedrooms of "([^"]*)"$/) do |num|
 	find_select ('minBeds')
-	select('2', :from => 'minBeds'); 
+	fill_in 'minBeds', :with=> num
 end
-And(/^I select house as the property type$/) do
+And (/^I select house as the property type$/) do
 	find_button ('data-rf-test-name="uipt1"')
 	click_button ('data-rf-test-name="uipt1"')
 	click_button ('apply-search-options')
 end
-Then(/^I should see three filter options applied to my search$/) do
+Then (/^I should see three filter options applied to my search$/) do
 	page.find('wideSidepaneFilterButtonContainer', '$0-$500k, 2+ beds, house')
 end
 	
